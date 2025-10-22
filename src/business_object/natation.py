8cb6c1014c6cb450b5a876dacaee1b10cd400b64
raphcrre <raphael.carrere@eleve.ensai.fr>
2025-10-22 10:01:26 +0000

    modif des classe cyclisme, natation et course

--- src/business_object/natation.py
+++ src/business_object/natation.py
@@ -3,14 +3,21 @@ from business_object.activite import Activite
 
 
 class Natation(Activite):
-    def __init__(self, id_activite: int, id_user: int, date: date, type_sport: str,
-                 distance: float, duree: timedelta, trace: str, id_parcours: int,
-                 titre: str, description: str,
-                 id_natation: int, vitesse_min100m: float = 0):
-        super().__init__(id_activite, id_user, date, type_sport, distance, duree,
-                         trace, id_parcours, titre, description)
-        self.id_natation = id_natation
-        self.vitesse_min100m = vitesse_min100m
+    def __init__(self, id_activite: int, id_user: int, date: date, distance: float,
+                 duree: timedelta, trace: str, id_parcours: int,
+                 titre: str, description: str, denivele: float):
+        super().__init__(id_activite=id_activite,
+                         id_user=id_user,
+                         date=date,
+                         type_sport="natation",
+                         distance=distance,
+                         duree=duree,
+                         trace=trace,
+                         id_parcours=id_parcours,
+                         titre=titre,
+                         description=description)
+        self.denivele = denivele
+        self.vitesse_min100m = None
 
     def afficher_details(self):
         print(f"Natation: {self.titre}, Distance: {self.distance} m, "
