9d0688d2997e65760177129785545423c03b9022
ClaraBvs <clara.shopping37@gmail.com>
2025-10-22 08:51:01 +0000

    methode user_dao

--- src/dao/user_dao.py
+++ src/dao/user_dao.py
@@ -46,3 +46,81 @@ class UserDao(metaclass=Singleton):
             created = True
 
         return created
+
+    def update(self, user) -> bool:
+        """Mise à jour d'un utilisateur"""
+        res = None
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        """
+                        UPDATE app.users
+                        SET prenom = %(prenom)s,
+                            nom = %(nom)s,
+                            username = %(username)s,
+                            mot_de_passe = %(mot_de_passe)s
+                        WHERE id_user = %(id_user)s
+                        RETURNING id_user;
+                        """,
+                        {
+                            "prenom": user.prenom,
+                            "nom": user.nom,
+                            "username": user.username,
+                            "mot_de_passe": user.mot_de_passe,
+                            "id_user": user.id_user,
+                        },
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+
+        updated = False
+        if res:
+            updated = True
+        return updated
+
+    def delete(self, id_user: int) -> bool:
+        """Supprime un utilisateur par son ID"""
+        res = None
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "DELETE FROM app.users WHERE id_user = %(id_user)s RETURNING id_user;",
+                        {"id_user": id_user},
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+
+        deleted = False
+        if res:
+            deleted = True
+        return deleted
+
+    def read(self, id_user: int) -> User | None:
+         """Récupère toutes les informations d'un utilisateur à partir de son ID"""
+         res = None
+
+        try:
+            with DBConnection().connection as connection:
+             with connection.cursor() as cursor:
+                cursor.execute(
+                    "SELECT * FROM app.users WHERE id_user = %(id_user)s;",
+                    {"id_user": id_user},
+                )
+                res = cursor.fetchone()
+        except Exception as e:
+        logging.info(e)
+
+        user = None
+        if res:
+            user = User(
+                id_user=res["id_user"],
+                prenom=res["prenom"],
+                nom=res["nom"],
+                username=res["username"],
+                mot_de_passe=res["mot_de_passe"],
+            )
+        return user
