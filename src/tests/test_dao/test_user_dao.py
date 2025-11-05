b807ca1fc537ce74f280ffd653d8393afac30bf6
ClaraBvs <clara.shopping37@gmail.com>
2025-11-05 11:46:24 +0000

    changes clara

--- src/tests/test_dao/test_user_dao.py
+++ src/tests/test_dao/test_user_dao.py
@@ -1,27 +1,21 @@
 import os
 import pytest
-
 from unittest.mock import patch
 from dotenv import load_dotenv
-from utils.reset_database import ResetDatabase
 from dao.user_dao import UserDao
 from business_object.user import User
-dotenv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", ".env")
-load_dotenv(dotenv_path)
 
-
-@pytest.fixture(scope="session", autouse=True)
-def setup_test_environment():
-    """Initialisation des données de test"""
-    with patch.dict(os.environ, {"SCHEMA": "projet_test_dao"}):
-        ResetDatabase().lancer(test_dao=True)
-        yield
 
 def test_creer_ok():
     """Création d'utilisateur réussie"""
 
     # GIVEN
-    user = User(prenom="clara", nom="beauvais", username="123456789", mot_de_passe="secret123")
+    user = User(prenom="clara", nom="beauvais", username="1234", mot_de_passe="secret123")
+
+    # Si l'utilisateur existe déjà, on le supprime
+    existing_user = UserDao().trouver_par_username(user.username)
+    if existing_user:
+        UserDao().supprimer(existing_user.id_user)
 
     # WHEN
     creation_ok = UserDao().creer(user)
